361bcee825e727311803407042994a7fd190ebe3
Red-K0 <114494949+Red-K0@users.noreply.github.com>
2026-08-13 05:31:34 +0300

    Add BinaryReader/BinaryWriter extensions for struct handling.

---
+++ src/Nimble/_system/IO/BinaryReader.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace System.IO;
+
+/// <summary>
+///     Extension methods for <see cref="BinaryReader"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class BinaryReaderExtensions
+{
+    extension(BinaryReader reader)
+    {
+        /// <summary>
+        ///     Reads a value of <see langword="unmanaged"/> type <typeparamref name="T"/> from the current stream and advances the current position by that number of bytes.
+        /// </summary>
+        /// <returns>A value of <see langword="unmanaged"/> type <typeparamref name="T"/> read from the current stream.</returns>
+        /// <exception cref="EndOfStreamException">The end of the stream is reached.</exception>
+        /// <exception cref="ObjectDisposedException">The stream is closed.</exception>
+        /// <exception cref="IOException">An I/O error occurs.</exception>
+        public T ReadStruct<T>() where T : unmanaged
+        {
+#if NET6_0_OR_GREATER
+            T value = new();
+
+            unsafe
+            {
+                reader.ReadExactly(new(&value, sizeof(T)));
+            }
+
+            return value;
+#else
+            byte[] bytes = reader.ReadBytes(sizeof(T));
+
+            if (bytes.Length != sizeof(T))
+                throw new EndOfStreamException("Unable to read beyond the end of the stream.");
+
+            fixed (byte* ptr = bytes)
+                return Marshal.PtrToStructure<T>((IntPtr)ptr);
+#endif
+        }
+
+        /// <summary>
+        ///     Reads the specified number of elements from the current stream and advances the current position by the read number of bytes.
+        /// </summary>
+        /// <param name="count">The number of bytes to read. This value must be 0 or a non-negative number or an exception will occur.</param>
+        /// <returns>An array containing data read from the underlying stream.</returns>
+        /// <exception cref="IOException">An I/O error occurs.</exception>
+        /// <exception cref="ObjectDisposedException">The stream is closed.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="count"/> is negative.</exception>
+        public T[] ReadStructs<T>(int count) where T : unmanaged
+        {
+#if NET6_0_OR_GREATER
+            Span<T> store = stackalloc T[count];
+
+            reader.ReadExactly(MemoryMarshal.AsBytes(store));
+
+            return store.ToArray();
+#else
+            int readLength = checked(sizeof(T) * count);
+
+            byte[] bytes = reader.ReadBytes(readLength);
+
+            if (bytes.Length != readLength)
+                throw new EndOfStreamException("Unable to read beyond the end of the stream.");
+
+            T[] elements = new T[count];
+
+            fixed (byte* b = bytes)
+            fixed (T* t = elements)
+            {
+                unsafe
+                {
+                    Buffer.MemoryCopy(b, t, readLength, readLength);
+                }
+            }
+
+            return elements;
+#endif
+        }
+    }
+}
---
+++ src/Nimble/_system/IO/BinaryWriter.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+
+#if NET6_0_OR_GREATER
+using System.Runtime.InteropServices;
+#endif
+
+namespace System.IO;
+
+/// <summary>
+///     Extension methods for <see cref="BinaryWriter"/>.
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class BinaryWriterExtensions
+{
+    extension(BinaryWriter writer)
+    {
+        /// <summary>
+        ///     Writes a value of <see langword="unmanaged"/> type <typeparamref name="T"/> to the underlying stream.
+        /// </summary>
+        /// <param name="source">The <see langword="unmanaged"/> <see langword="struct"/> to write.</param>
+        /// <exception cref="IOException">An I/O error occurs.</exception>
+        /// <exception cref="ObjectDisposedException">The stream is closed.</exception>
+        public void WriteStruct<T>(T source) where T : unmanaged
+        {
+#if NET6_0_OR_GREATER
+            unsafe
+            {
+                writer.Write(new ReadOnlySpan<byte>(&source, sizeof(T)));
+            }
+#else
+            byte[] bytes = new byte[sizeof(T)];
+
+            fixed (byte* dst = bytes)
+            {
+                unsafe
+                {
+                    Buffer.MemoryCopy(&source, dst, sizeof(T), sizeof(T));
+                }
+            }
+
+            writer.Write(bytes);
+#endif
+        }
+
+        /// <summary>
+        ///     Writes an array of <see langword="unmanaged"/> type <typeparamref name="T"/> to the underlying stream.
+        /// </summary>
+        /// <param name="values">An array containing the values to write.</param>
+        /// <exception cref="IOException">An I/O error occurs.</exception>
+        /// <exception cref="ObjectDisposedException">The stream is closed.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="values"/> is <see langword="null"/>.</exception>
+#if NET6_0_OR_GREATER
+        public void WriteStructs<T>(ReadOnlySpan<T> values) where T : unmanaged
+        {
+            writer.Write(MemoryMarshal.AsBytes(values));
+        }
+#else
+        public void WriteStructs<T>(T[] values) where T : unmanaged
+        {
+            ArgumentNullException.ThrowIfNull(values);
+
+            foreach (T value in values)
+                writer.WriteStruct(value);
+        }
+#endif
+    }
+}
